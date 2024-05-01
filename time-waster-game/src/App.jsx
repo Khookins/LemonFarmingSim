f167a34d6099fb45956596663a1d5d7fa5da20cd
Khookins <131645212+Khookins@users.noreply.github.com>
2024-05-01 11:19:31 +1000

    Fixed stuff - margin - cool click level thingy - um more stuff

--- time-waster-game/src/App.jsx
+++ time-waster-game/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     localStorage.setItem("lemons", String(count));
     localStorage.setItem("lemonHarvestLevel", lemonHarvestLevel);
-    setHarvestAmount(harvestAmount + lemonHarvestLevel); // More additions to harvest amount later
+    setHarvestAmount(1 + lemonHarvestLevel); // More additions to harvest amount later
 
     console.log(harvestAmount);
     console.log(lemonHarvestLevel);
@@ -28,24 +28,28 @@ function App() {
       <h1>{"Your Lemons: " + count}</h1>
       <div className="card">
         <button
-          onClick={() =>
-            setCount((count) => Number(count) + Number(harvestAmount))
-          }
+          onClick={() => {
+            console.log(
+              "I COUNTING " + count + " AND SILLY NUMBER " + harvestAmount
+            );
+            setCount((count) => Number(count) + Number(harvestAmount));
+          }}
         >
           count is {count}
         </button>
         <button
           onClick={() => {
-            if (count <= 500) return;
+            if (count < 500) return;
             setLemonHarvestLevel(Number(lemonHarvestLevel) + 1);
+            console.log(
+              "I SET THE SILLY LEVEL TO " + lemonHarvestLevel + "+" + 1
+            );
             setCount((count) => Number(count) - 500);
           }}
         >
           Upgrade Lemon Harvest (500)
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>You're Getting {harvestAmount} Lemons Per Click</p>
       </div>
       <p
         onClick={() => {
